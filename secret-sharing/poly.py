0a2639aadb7d4512b98c6a735465a47d1977aff4
applexi <xi.aprille@gmail.com>
2026-06-17 14:51:01 -0700

    base tests passed

---
+++ secret-sharing/poly.py
@@ -0,0 +1,23 @@
+from crypto import CryptoEnv
+from sympy import isprime
+
+
+def eval_poly(coeffs : list, val : int, env: CryptoEnv) -> int:
+    val = val % env.q
+    d = len(coeffs) - 1
+    p = coeffs[d]
+    for i in range(d - 1, -1, -1):
+        p = (p * val + coeffs[i]) % env.q
+    return p
+
+def lagrange_interp(val : int, points : list[tuple[int, int]], env : CryptoEnv) -> int:
+    assert isprime(env.q)
+    R = [0] * len(points)
+    for i, (a_i, b_i) in enumerate(points):
+        num, den = 1, 1
+        for j, (a_j, _) in enumerate(points):
+            if i != j:
+                num = (num * (val - a_j)) % env.q
+                den = (den * (a_i - a_j)) % env.q
+        R[i] = (b_i * num * pow(den, -1, env.q)) % env.q
+    return sum(R) % env.q
---
+++ secret-sharing/schemes.py
@@ -0,0 +1,76 @@
+from crypto import CryptoEnv
+from poly import eval_poly, lagrange_interp
+from itertools import combinations
+from sympy import isprime
+import secrets
+
+class additive:
+    @staticmethod
+    def share(s : int, env: CryptoEnv) -> list[int]:
+        assert 0 <= s < env.q
+        a = [secrets.randbelow(env.q) for _ in range(env.n - 1)]
+        a_n = (s - sum(a)) % env.q
+        shares = a + [a_n]
+        return shares
+    
+    @staticmethod
+    def reconstruct(shares: list[int], env: CryptoEnv) -> int:
+        assert len(shares) == env.n
+        s_prime = sum(shares) % env.q
+        return s_prime
+
+class rss:
+    @staticmethod
+    def share(s : int, env: CryptoEnv) -> dict[int, dict[tuple[int, ...], int]]:
+        assert 0 <= s < env.q
+        assert 1 <= env.k <= env.n
+        Alpha = combinations(range(1, env.n + 1), env.k - 1)
+        a_A, a_sum = {}, 0
+        prev_A = next(Alpha)
+        for A in Alpha:
+            a_A[prev_A] = secrets.randbelow(env.q)
+            a_sum = (a_sum + a_A[prev_A]) % env.q
+            prev_A = A
+        a_A[prev_A] = (s - a_sum) % env.q
+        shares = {i: {} for i in range(1, env.n + 1)}
+        for A, a in a_A.items():
+            not_in = set(range(1, env.n + 1)) - set(A)
+            for i in not_in:
+                shares[i][A] = a
+
+        return shares
+    
+    @staticmethod
+    def reconstruct(shares: dict[int, dict[tuple[int, ...], int]], env: CryptoEnv) -> int:
+        assert len(shares) == env.k
+        Alpha = combinations(range(1, env.n + 1), env.k - 1)
+        s_prime = 0
+        for A in Alpha:
+            for j, share_j in shares.items():
+                if j not in A:
+                    a = share_j[A]
+                    s_prime = (s_prime + a) % env.q
+                    break
+            else: 
+                raise ValueError(f"Missing share for subset {A}")
+        return s_prime
+
+class shamir:
+    @staticmethod
+    def share(s : int, env: CryptoEnv) -> list[int]:
+        assert 0 <= s < env.q
+        assert 1 <= env.k <= env.n
+        assert env.q > env.n
+        a = [secrets.randbelow(env.q) for _ in range(env.k - 1)]
+        a[-1] = secrets.randbelow(env.q - 1) + 1
+        a[0] = s
+        shares = [eval_poly(a, i, env) for i in range(1, env.n + 1)]
+        return shares
+    
+    @staticmethod
+    def reconstruct(shares: list[int], env: CryptoEnv) -> int:
+        assert isprime(env.q)
+        assert len(shares) >= env.k
+        points = [(i + 1, share) for i, share in enumerate(shares)]
+        s_prime = lagrange_interp(0, points, env)
+        return s_prime
