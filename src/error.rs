d36b54f07a55297b33e5fc3218ada698872fa57b
applexi <aprille.xi@toolsforhumanity.com>
2026-08-04 15:50:24 -0700

    replicated ss implemented

---
+++ src/error.rs
@@ -0,0 +1,6 @@
+#[derive(Debug)]
+pub enum Error {
+    String(String),
+    Rng,
+}
+
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod sharing;
+pub mod error;
+
+pub const DEFAULT_N: usize = 3;
+pub const DEFAULT_K: usize = 2;
